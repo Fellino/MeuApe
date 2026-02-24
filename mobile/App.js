d9081ce05c3a48ffcbf8129cebce8ecd10d13547
Erick Castilho <erick.jansons@hotmail.com>
2026-02-23 21:45:18 -0300

    Tela de login básica para teste

--- mobile/App.js
+++ mobile/App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { View, Text, TextInput, Pressable, Alert } from 'react-native';
+import { api } from './src/services/api';
 
 export default function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleLogin() {
+    try {
+      const response = await api.post('/auth/login', {
+        email,
+        password
+      });
+
+      console.log(response.data);
+      Alert.alert('Sucesso', 'Login realizado');
+    } catch (error) {
+      console.log(error.response?.data);
+      Alert.alert('Erro', 'Falha no login');
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ padding: 20, marginTop: 100 }}>
+      <Text>Email</Text>
+      <TextInput
+        value={email}
+        onChangeText={setEmail}
+        style={{ borderWidth: 1, marginBottom: 10, borderRadius: 10 }}
+      />
+
+      <Text> Senha</Text>
+      <TextInput
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        style={{ borderWidth: 1, marginBottom: 20, borderRadius: 10 }}
+      />
+
+      <Pressable
+        onPress={handleLogin}
+        style={{ backgroundColor: 'black', padding: 10, borderRadius: 20 }}
+      >
+        <Text style={{ color: 'white', textAlign: 'center' }}>
+          Entrar
+        </Text>
+      </Pressable>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
